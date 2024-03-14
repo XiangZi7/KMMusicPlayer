a80dc25f8a537671736267d1e118fb9034d8acf4
XiangZi7 <793923048@qq.com>
2024-03-14 17:18:25 +0800

    删除不必要代码

---
+++ src/hooks/AudioError.ts
@@ -0,0 +1,6 @@
+export const errorMessages = {
+    MEDIA_ERR_ABORTED: "播放被终止。",
+    MEDIA_ERR_NETWORK: "下载过程中发生网络错误。",
+    MEDIA_ERR_DECODE: "解码过程中发生错误。",
+    MEDIA_ERR_SRC_NOT_SUPPORTED: "音频格式不支持。",
+};
