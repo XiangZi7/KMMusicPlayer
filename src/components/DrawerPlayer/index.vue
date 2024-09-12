e4234bc2c0cbbd038b4d7f58eea3a3e77ae07e66
XiangZi7 <793923048@qq.com>
2024-09-12 13:46:10 +0800

    🐞 fix: 回复图片

--- src/components/DrawerPlayer/index.vue
+++ src/components/DrawerPlayer/index.vue
@@ -121,7 +121,7 @@ defineExpose({
 <template>
   <el-drawer
     :style="{
-      '--track-cover-url': `url(https://q0.itc.cn/q_70/images03/20240814/2544ea72400a472798ad81610ca4440e.jpeg)`,
+      '--track-cover-url': `url(${SettingStore.isDrawerCover ? currentSong.cover : ''})`,
     }"
     v-model="drawer"
     :direction="direction"
